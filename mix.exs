c1f423ffab7bd4823861fa5d767d0e95917108d0
Roman Chvanikov <chvanikoff@gmail.com>
2016-09-10 03:52:19 +0300

    package info

--- mix.exs
+++ mix.exs
@@ -2,33 +2,36 @@ defmodule VKReq.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :vkreq,
-     version: "0.0.1",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :vkreq,
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      package: package(),
+      description: """
+      VKontakte request validation plug
+      """
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :plug]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:plug, "~> 1.2"}
+      {:plug, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Roman Chvanikov"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/chvanikoff/vkreq"}
     ]
   end
 end
